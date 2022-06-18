bf7f58168fb782f0425a9d350acd0335a857d065
wisdom1230 <694961699@qq.com>
2022-06-18 11:40:59 +0800

    kpi内容
    添加部门kpi内容

--- apis/content.go
+++ apis/content.go
@@ -1,1 +1,18 @@
+package apis
 
+var Department = map[string]string{
+
+	"销售部":     "Sales",
+	"供应链管理中心": "SupplyChainManagement",
+	"研究与合作中心": "ResearchCollaborationCenter",
+	"财务管理中心":  "FinancialManagementCenter",
+	"人力资源中心":  "HumanResourceCenter",
+	"产能利用中心":  "CapacityUtilizationCenter",
+
+	"Sales":                       "销售部",
+	"SupplyChainManagement":       "供应链管理中心",
+	"FinancialManagementCenter":   "财务管理中心",
+	"ResearchCollaborationCenter": "研究与合作中心",
+	"HumanResourceCenter":         "人力资源中心",
+	"CapacityUtilizationCenter":   "产能利用中心",
+}
